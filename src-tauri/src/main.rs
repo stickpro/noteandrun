19371cc9034a0701fdeadfffa06aa24ad25a3aef
stickpro <stick.qwe@gmail.com>
2023-10-19 10:41:52 +0300

    test bonsaidb

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,63 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::time::SystemTime;
+
+use bonsaidb::core::schema::{Collection, SerializedCollection};
+use bonsaidb::local::config::{Builder, StorageConfiguration};
+use bonsaidb::local::Database;
+use serde::{Deserialize, Serialize};
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn save_text(text: String) -> Result<(), bonsaidb::core::Error> {
+
+    let db = Database::open::<BlogPost>(StorageConfiguration::new("my-db.bonsaidb"))?;
+
+    let document = Message {
+        contents: String::from(text),
+        timestamp: SystemTime::now(),
+    }
+        .push_into(&db)?;
+
+    let message_doc =
+        Message::get(&document.header.id, &db)?.expect("couldn't retrieve stored item");
+
+    println!(
+        "Inserted message '{:?}' with id {}",
+        message_doc.contents, message_doc.header.id
+    );
+
+    Ok(())
 }
+
+#[derive(Debug, Serialize, Deserialize, Collection)]
+#[collection(name = "blog_posts")]
+struct BlogPost {
+    pub title: String,
+    pub contents: String,
+}
+
+
+#[derive(Debug, Serialize, Deserialize, Collection)]
+#[collection(name = "blog_posts")]  // Set the collection name
+struct BlogPostCollection {
+    #[serde(rename = "title")] // Map the field to the collection
+    pub title: String,
+    #[serde(rename = "contents")] // Map the field to the collection
+    pub contents: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Collection)]
+#[collection(name = "messages")]
+struct Message {
+    pub timestamp: SystemTime,
+    pub contents: String,
+}
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![save_text])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/EditorLayout/Editor/index.tsx
+++ src/components/EditorLayout/Editor/index.tsx
@@ -1,9 +1,15 @@
+import { invoke } from '@tauri-apps/api'
 
+const saveText = () => {
+    console.log('save-text-js')
+    invoke('save_text',  { text: 'World' }).then(r => console.log(r))
+}
 const Editor = () => {
     return (
         <div>
             <textarea
                 />
+            <button onClick={saveText}>save</button>
         </div>
     )
 }
